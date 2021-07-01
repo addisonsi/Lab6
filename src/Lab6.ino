60c31a6e7618e57d8e1e366f102affa7812d574e
Addison Simon <addisonsimon@Addisons-MacBook-Air.local>
2021-07-01 13:37:09 -0500

    first commit

---
+++ src/Lab6.ino
@@ -0,0 +1,41 @@
+#include "oled-wing-adafruit.h"
+/*
+ * Project Lab_6
+ * Description:
+ * Author:
+ * Date:
+ */
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+// setup() runs once, when the device is first turned on.
+OledWingAdafruit display;
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  display.setup();
+  display.clearDisplay();
+  display.display();  // cause the display to be updated
+  pinMode(A4, INPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+  display.clearDisplay();
+  display.loop();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+  double voltage = (analogRead(A4) * 3.3) / 4095.0;
+  double temperature = (voltage - 0.5) * 100;
+
+  display.print("Temperature:");
+  display.print((int)(temperature+0.5));
+  display.println(" degrees Celsius");
+  display.print("Temperature: ");
+  display.print((int)(temperature*9/5 + 32 + 0.5));
+  display.println(" degrees Farenheit");
+  display.display();
+  delay(1000);
+}
